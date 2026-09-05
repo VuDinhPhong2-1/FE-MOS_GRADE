6be379e432049039948c1d31e7312dd45727b1e1
VuDinhPhong2-1 <vudinhphong.261001@gmail.com>
2026-09-05 09:29:27 +0700

    Update "xem trước"

--- src/components/InsertedImageEditor.tsx
+++ src/components/InsertedImageEditor.tsx
@@ -331,11 +331,11 @@ const InsertedImageEditor = ({
         </p>
 
         {previewUrl ? (
-          <div className="relative flex min-h-16 items-center justify-center rounded-xl border border-slate-200 bg-white p-2.5">
+          <div className="relative flex min-h-[500px] max-h-[500px] w-full items-center justify-center rounded-xl border border-slate-200 bg-white p-2.5">
             <img
               src={previewUrl}
               alt="Inserted image preview"
-              className="max-h-10 max-w-10 object-contain"
+              className="max-h-[500px] max-w-[500px] object-contain"
             />
 
             {uploading && (
--- src/components/PictureBulletEditor.tsx
+++ src/components/PictureBulletEditor.tsx
@@ -323,11 +323,11 @@ const PictureBulletEditor = ({
         </p>
 
         {previewUrl ? (
-          <div className="relative flex min-h-16 items-center justify-center rounded-xl border border-slate-200 bg-white p-2.5">
+          <div className="relative flex items-center justify-center rounded-xl border border-slate-200 bg-white p-2.5">
             <img
               src={previewUrl}
               alt="Picture bullet preview"
-              className="max-h-10 max-w-10 object-contain"
+              className="max-h-[500px] max-w-[500px] object-contain"
             />
 
             {uploading && (
@@ -358,7 +358,7 @@ const PictureBulletEditor = ({
           </div>
         )}
       </div>
-    </div>
+    </div >
   );
 };
 
